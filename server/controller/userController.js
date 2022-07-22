d683c2a388f08b25fea344788e50167b64630d29
Denis Kozlenkov <kozelden4ik@gmail.com>
2022-07-22 14:57:15 +0300

    login

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,12 +1,28 @@
 require('dotenv').config();
 const bcrypt = require('bcrypt');
-const { json } = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('../model/User.js');
 
-class userController {
-    async login(){
+const generateToken = (id, username) => {
+    return jwt.sign({id, username},
+         process.env.secret, 
+         {expiresIn: "24h"})
+}
 
+class userController {
+    async login(req, res){
+        try {
+            const{username, password} = req.body;
+            const user = await User.findOne({username});
+            if(!user) res.status(400).json({message: `User with this username ${username} not found`});
+            const validPass = bcrypt.compareSync(password, user.password);
+            if(!validPass) res.status(400).json({message: `Inccorect Password`});
+            const token = generateToken(user._id, user.username);
+            return res.json({token})
+        } catch (error) {
+            console.log(error);
+            return res.status(400).json({message: 'Login error'});
+        }
     };
     async reg(req, res){
         try {
--- server/router/authRouter.js
+++ server/router/authRouter.js
@@ -1,9 +1,13 @@
 const {Router} = require('express');
+const check = require('express-validator');
 const userController = require('../controller/userController.js');
 
 const authRouter = new Router();
 
-authRouter.post('/reg', userController.reg);
+authRouter.post('/reg',[
+        check('username','Username cannot be empty').notEmpty(),
+        check('password', 'Password must be longer than 4 symbols and shorter than 10').isLength({min: 4, max: 10})
+    ], userController.reg);
 authRouter.post('/login', userController.login);
 
 module.exports = authRouter;
